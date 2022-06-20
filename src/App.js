1f7121cd7d6d3108798ff71243fb1df2d7139388
Crimson Tiangco <crimson.tiangco@sollertiainc.com>
2022-06-20 22:04:02 +0800

    added incorrect credentials alert

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ function App() {
       .catch(function (error) {
         console.log(error.response.data);
         setLoader(false);
+        alert("Incorrect Username or Password");
       });
 
   }
